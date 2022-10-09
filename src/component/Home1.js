9e78531c179caade47c35a94a49d64e08dc21478
sunil7458 <115408245+sunil7458@users.noreply.github.com>
2022-10-09 18:54:13 +0530

    Update Home1.js

--- src/component/Home1.js
+++ src/component/Home1.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 // import { Link } from "react-router-dom";
-import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Product from "./Product";
 import axios from "axios";
 import Nav from "./Nav";
@@ -9,7 +7,7 @@ import Product1 from "./Product1";
 import Footer from "./Footer";
 
 const Home1 = () => {
-  const [mobile, setmobile] = useState(false);
+  
   const [filterout, setfilterout] = useState();
 
   useEffect(() => {
